a83d5ba60a4fc7838e577b604f6bfc4ea49d7029
João Gabriel Macedo <jgabreumacedo@gmail.com>
2023-10-25 17:30:51 -0300

    Project init and Header on main page

---
+++ MacedoViagens/HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  MacedoViagens
+//
+//  Created by Joao Gabriel Macedo on 25/10/23.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
